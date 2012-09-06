8e029aec5e8f82d41548bd72cc9e2ad5e11757ac
Jeremy Bicha <jbicha@ubuntu.com>
2012-09-06 16:09:15 -0400

    oops, forgot a few .text's

--- setup.sh
+++ setup.sh
@@ -10,9 +10,9 @@ echo -e "not many\nlines\nhere" > few-lines
 echo -e '#\!/bin/bash\nfor i in {1..10}; do echo $i ; sleep 0.25 ;  echo "error" >&2 ; done' > erroring-script.sh
 chmod +x erroring-script.sh
 mkdir globbing
-touch globbing/a{1..5}.text globbing/a{20..22}.txt
-touch globbing/b{5..10}.text globbing/b{17..26}.txt
-touch globbing/c{3..7}.text globbing/c{25..27}.txt
+touch globbing/a{1..5}.txt globbing/a{20..22}.txt
+touch globbing/b{5..10}.txt globbing/b{17..26}.txt
+touch globbing/c{3..7}.txt globbing/c{25..27}.txt
 touch globbing/d{6..12}.txt
 touch globbing/a{10..15}.txt
 touch globbing/a{1,3,5,7}.jpg
